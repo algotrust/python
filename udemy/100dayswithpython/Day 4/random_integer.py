2e5cfd136d40c43a40585182957b01623db45ce0
wickedpuppy@gmail.com <Rqfn7g2b@Y0U>
2021-12-11 08:08:32 +0800

    day 5
    day 5

--- udemy/100dayswithpython/Day 4/random_integer.py
+++ udemy/100dayswithpython/Day 4/random_integer.py
@@ -1,4 +1,8 @@
 import random
+#import my_module
+#print(my_module.pi)
 
 random_integer = random.randint(1,10)
-print(f"The random integer is : {random_integer}")
+random_float = random.random()
+print(f"The random integer is : {random_integer}\n")
+print(f"The random float is : {random_float}")
