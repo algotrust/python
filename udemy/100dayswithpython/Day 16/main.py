33b550303720b7664105f2120c3e9e60fe270efa
wickedpuppy@gmail.com <Rqfn7g2b@Y0U>
2022-01-02 21:05:28 +0800

    day 17
    day 17

--- udemy/100dayswithpython/Day 16/main.py
+++ udemy/100dayswithpython/Day 16/main.py
@@ -1,9 +1,18 @@
-import turtle
+# import turtle
+#
+# timmy = turtle.Turtle()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+#
+# my_screen = turtle.Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = turtle.Turtle()
-timmy.shape("turtle")
-timmy.color("red")
+from prettytable import PrettyTable
 
-my_screen = turtle.Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtel", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
